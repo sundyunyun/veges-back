4a2d449cfc1e4d55851f3c140748458203ea7153
sunyunyun <sundyunyun@163.com>
2018-03-20 16:35:39 +0800

    mapper change

---
+++ src/main/java/dhu/sun/vege/mapper/OrderItemMapper.java
@@ -0,0 +1,10 @@
+package dhu.sun.vege.mapper;
+
+import dhu.sun.vege.entity.OrderItem;
+import dhu.sun.vege.util.core.BaseMapper;
+
+/**
+ * Created by think on 2018/3/20.
+ */
+public interface OrderItemMapper extends BaseMapper<OrderItem> {
+}
---
+++ src/main/java/dhu/sun/vege/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package dhu.sun.vege.mapper;
+
+import dhu.sun.vege.entity.Order;
+import dhu.sun.vege.util.core.BaseMapper;
+
+/**
+ * Created by think on 2018/3/20.
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+}
